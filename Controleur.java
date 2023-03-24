e851879ad001fd35b66877cde4d26ade8f0cf6c2
EstebanBreaHell <esteban.breahell@gmail.com>
2023-03-24 11:23:32 +0100

    début

---
+++ Controleur.java
@@ -0,0 +1,31 @@
+import java.awt.Shape;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+public class Controleur {
+    
+    JFrame ihm;
+
+    ArrayList<Shape> arrForme; 
+
+    public Controleur() {
+        arrForme = new ArrayList<Shape>();
+        ihm = new Frame(this);
+        
+
+        
+    }
+
+    public static void main(String[] args) {
+        Controleur c = new Controleur();
+    }
+
+
+    
+
+
+
+
+
+}
