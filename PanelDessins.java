69a878774a1f05324124056fd9c0c0edf8634d9b
EstebanBreaHell <esteban.brea-hell@etu.univ-lehavre.fr>
2023-03-26 21:32:38 +0200

    preview

--- PanelDessins.java
+++ PanelDessins.java
@@ -2,24 +2,30 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 import java.awt.Color;
 
-public class PanelDessins extends JPanel implements MouseListener , ActionListener
+public class PanelDessins extends JPanel implements MouseListener , ActionListener , MouseMotionListener
 {
 
-    int baseX, baseY , newX , newY;
-    JFrame frame;
-    Controleur c;
+    private int baseX, baseY , newX , newY;
+    private JFrame frame;
+    private Controleur c;
+    private Boolean isPressed;
     public PanelDessins(JFrame frame, Controleur c)
     {
         this.frame = frame;
         this.c = c;
+        this.isPressed = false;
+        
         this.setSize(500, 500);
         this.setVisible(true);
         this.setBackground(Color.white);
         this.addMouseListener(this);
+        this.addMouseMotionListener(this);
 
         this.baseX = this.baseY = this.newX = this.newY = 0;
 
@@ -30,20 +36,90 @@ public class PanelDessins extends JPanel implements MouseListener , ActionListen
 
     public void paintComponent(Graphics g)
     {
-        super.paintComponent(g);
+        super.paintComponent(g); 
         g.setColor(Color.blue);
 
         ArrayList<Forme> arrForme = this.c.getArrForme();
 
         for (Forme f : arrForme){
             if (f.getTypeForme().equals("Cercle")){
-                g.drawOval(f.getXDebut(), f.getYDebut(), f.getXFin()-f.getXDebut(),f.getYFin() - f.getYDebut());
+                int x2 = f.getXFin() - f.getXDebut();
+                int y2 = f.getYFin() - f.getYDebut();
+
+                if (x2 >0 && y2 > 0){
+                    g.drawOval(f.getXDebut(), f.getYDebut(), x2,y2);
+                }
+                else if (x2 < 0 && y2 < 0){
+                    g.drawOval(f.getXFin(), f.getYFin(), -x2,-y2);
+                }
+                else if (x2 < 0 && y2 > 0){
+                    g.drawOval(f.getXFin(), f.getYDebut(), -x2,y2);
+                }
+                else if (x2 > 0 && y2 < 0){
+                    g.drawOval(f.getXDebut(), f.getYFin(), x2,-y2);
+                }
             }
             else if (f.getTypeForme().equals("Rectangle")){
-                g.drawRect(f.getXDebut(), f.getYDebut(), f.getXFin()-f.getXDebut(),f.getYFin() - f.getYDebut());
+
+                int x2 = f.getXFin() - f.getXDebut();
+                int y2 = f.getYFin() - f.getYDebut();
+
+                if (x2 >0 && y2 > 0){
+                    g.drawRect(f.getXDebut(), f.getYDebut(), x2,y2);
+                }
+                else if (x2 < 0 && y2 < 0){
+                    g.drawRect(f.getXFin(), f.getYFin(), -x2,-y2);
+                }
+                else if (x2 < 0 && y2 > 0){
+                    g.drawRect(f.getXFin(), f.getYDebut(), -x2,y2);
+                }
+                else if (x2 > 0 && y2 < 0){
+                    g.drawRect(f.getXDebut(), f.getYFin(), x2,-y2);
+                }
+
             }
             else if (f.getTypeForme().equals("Ligne")){
                 g.drawLine(f.getXDebut(), f.getYDebut(), f.getXFin(), f.getYFin());
+            }
+        }
+
+        if (this.isPressed){
+            if (this.c.getTypeForme().equals("Cercle")){
+                int x2 = this.newX - this.baseX;
+                int y2 = this.newY - this.baseY;
+
+                if (x2 >0 && y2 > 0){
+                    g.drawOval(this.baseX, this.baseY, x2,y2);
+                }
+                else if (x2 < 0 && y2 < 0){
+                    g.drawOval(this.newX, this.newY, -x2,-y2);
+                }
+                else if (x2 < 0 && y2 > 0){
+                    g.drawOval(this.newX, this.baseY, -x2,y2);
+                }
+                else if (x2 > 0 && y2 < 0){
+                    g.drawOval(this.baseX, this.newY, x2,-y2);
+                }
+            }
+            else if (this.c.getTypeForme().equals("Rectangle")){
+                int x2 = this.newX - this.baseX;
+                int y2 = this.newY - this.baseY;
+
+                if (x2 >0 && y2 > 0){
+                    g.drawRect(this.baseX, this.baseY, x2,y2);
+                }
+                else if (x2 < 0 && y2 < 0){
+                    g.drawRect(this.newX, this.newY, -x2,-y2);
+                }
+                else if (x2 < 0 && y2 > 0){
+                    g.drawRect(this.newX, this.baseY, -x2,y2);
+                }
+                else if (x2 > 0 && y2 < 0){
+                    g.drawRect(this.baseX, this.newY, x2,-y2);
+                }
+            }
+            else if (this.c.getTypeForme().equals("Ligne")){
+                g.drawLine(this.baseX, this.baseY, this.newX, this.newY);
             }
         }
 
@@ -80,10 +156,21 @@ public class PanelDessins extends JPanel implements MouseListener , ActionListen
         this.baseX = e.getX();
         this.baseY = e.getY();
 
-        System.out.println("BASE X : " + this.baseX);
-        System.out.println("BASE Y : " + this.baseY);
+        this.isPressed = true;
 
 
+
+
+    }
+
+    public void mouseDragged(java.awt.event.MouseEvent e)
+    {
+        this.newX = e.getX();
+        this.newY = e.getY();
+
+        this.repaint();
+
+        
     }
 
     public void mouseReleased(java.awt.event.MouseEvent e)
@@ -91,13 +178,17 @@ public class PanelDessins extends JPanel implements MouseListener , ActionListen
         this.newX = e.getX();
         this.newY = e.getY();
 
-        System.out.println("NEW X : " + this.newX);
-        System.out.println("NEW Y : " + this.newY);
+        this.isPressed = false;
 
 
         this.c.creeForme(this.c.getTypeForme(), this.baseX, this.baseY, this.newX, this.newY, Color.BLUE, 1);
 
         this.repaint();
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        return;
     }
 
 
